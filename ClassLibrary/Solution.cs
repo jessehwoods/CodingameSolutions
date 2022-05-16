bf5a15da508a8e44c18ccfb5c2ee6db5ddaf5737
Jesse Woods <jessehwoods@gmail.com>
2022-05-16 09:25:27 -0400

    Added the provided code from the problem.

--- ClassLibrary/Solution.cs
+++ ClassLibrary/Solution.cs
@@ -1,7 +1,25 @@
 ﻿namespace ClassLibrary
-{
-    public class Solution
-    {
 
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string MESSAGE = Console.ReadLine();
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("answer");
     }
 }
