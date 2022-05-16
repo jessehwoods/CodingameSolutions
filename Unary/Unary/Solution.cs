747599438a6258629c103fb7670b634fc969a8a7
Jesse Woods <jessehwoods@gmail.com>
2022-05-16 10:04:58 -0400

    Reconfigured so that tests actually run. Issue was project type.

---
+++ Unary/Unary/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("UnaryTests")]
+
+namespace Unary
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SayBlah());
+        }
+
+        internal static string SayBlah()
+        {
+            return "blah";
+        }
+    }
+
+}
---
+++ UnaryTests/UnitTestProject1/Tests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnaryTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("blah", Unary.Solution);
+        }
+    }
+}
