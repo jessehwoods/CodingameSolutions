1aa455bfafa695c66f56e21fcba60649d0964c82
Jesse Woods <jessehwoods@gmail.com>
2022-05-16 12:32:08 -0400

    First pass at all the methods. Changed access, need to test each method.

--- Unary/Unary/Solution.cs
+++ Unary/Unary/Solution.cs
@@ -11,6 +11,12 @@ namespace Unary
 {
     internal class Solution
     {
+        //To be used for zero
+        private static char ZERO = '0';
+
+        //To be used for whitespace
+        private static char WHITESPACE = (char) 32;
+
         static void Main(string[] args)
         {
             //Gets the string to be converted
@@ -28,23 +34,73 @@ namespace Unary
          */
         internal static string ConvertStringToUnary(string StringToConvert)
         {
-            //Holds the binary representation of the string
-            StringBuilder BinaryVersion = new StringBuilder();
-
-            //Convert the string to a binary representation
-            foreach (char c in StringToConvert)
-            {
-                BinaryVersion.Append(Convert.ToString((int) c, 2));
-            }
-
-            //Holds the string that will be returned
-            StringBuilder ToReturn = new StringBuilder();
+            //Convert the string to a string of '1' and '0' chars based on the binary
+            string BinaryVersion = GetBinaryVersionOfString(StringToConvert);
 
             //Convert the binary to unary
+            string UnaryVersion = GetUnaryVersionOfBinary(BinaryVersion);
 
-            return ToReturn.ToString();
+            return UnaryVersion;
         }
 
+        /**
+         * Converts a string to a binary version of the string as a series of '1' and '0' characters with no whitespace.
+         */
+        internal static string GetBinaryVersionOfString(string CharStringToConvert)
+        {
+            StringBuilder BinaryVersion = new StringBuilder();
+
+            foreach (char c in CharStringToConvert)
+            {
+                BinaryVersion.Append(Convert.ToString((int)c, 2));
+            }
+
+            return BinaryVersion.ToString();
+        }
+
+        /**
+        * Converts a string to a unary version of the string as a series of '0' and whitespace characters.
+         */
+        internal static string GetUnaryVersionOfBinary(string BinaryStringToConvert)
+        {
+            StringBuilder UnaryVersion = new StringBuilder();
+
+            //Handle the first character by adding the first part of the unary expression
+            char CurrentChar = BinaryStringToConvert[0];
+            UnaryVersion.Append(LeadCharacter(CurrentChar));
+
+            //Convert the binary to an unary representation. Include the first character, because it needs to be counted for the second part of the
+            //unary expression.
+            foreach (char c in BinaryStringToConvert)
+            {
+                if (!c.Equals(CurrentChar))
+                {
+                    //If it's a new character, need to add the first part
+                    UnaryVersion.Append(WHITESPACE);
+                    UnaryVersion.Append(LeadCharacter(c));
+                }
+                //Always add a zero for each character to the second part.
+                UnaryVersion.Append(ZERO);
+            }
+
+            return UnaryVersion.ToString();
+        }
+
+        /**
+         * Gives you the correct firts part of an unary expression for the input character.
+         */
+        private static string LeadCharacter(char InputChar)
+        {
+            StringBuilder sb = new StringBuilder(3);
+            sb.Append(ZERO);
+            if (InputChar.Equals(ZERO))
+            {
+                sb.Append(ZERO);
+            }
+            sb.Append(WHITESPACE);
+
+            return sb.ToString();
+        }
     }
 
 }
