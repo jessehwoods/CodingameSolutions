4b5a85f731f9d4560d329512659700cfdd36dd8d
Jesse Woods <jessehwoods@gmail.com>
2022-05-16 10:17:54 -0400

    Finished tests cases provided on site.

--- UnaryTests/UnaryTests/UnitTest1.cs
+++ UnaryTests/UnaryTests/UnitTest1.cs
@@ -7,40 +7,44 @@ namespace UnaryTests
     [TestClass]
     public class UnitTest1
     {
-        // Unary equivalent of the string "C"
+        //String "C" input and converted form
+        private static string CINPUT = "C";
         private static string CSOLUTION = "0 0 00 0000 0 00";
 
-        // Unary equivalent of the string "CC"
+        //String "CC" input and converted form
+        private static string CCINPUT = "CC";
         private static string CCSOLUTION = "0 0 00 0000 0 000 00 0000 0 00";
 
-        // Unary equivalent of the string "%"
+        //String "%" input and converted form
+        private static string PERCENTINPUT = "%";
         private static string PERCENTSOLUTION = "00 0 0 0 00 00 0 0 00 0 0 0";
 
-        // Unary equivalent of the string "Chuck Norris' keyboard has 2 keys: 0 and white space."
+        //String "Chuck Norris' keyboard has 2 keys: 0 and white space." input and converted form
+        private static string MESSAGEINPUT = "Chuck Norris' keyboard has 2 keys: 0 and white space.";
         private static string MESSAGESOLUTION = "0 0 00 0000 0 0000 00 0 0 0 00 ...0 000 00 000 0 0000 00 00 0 0 00 0 0 0 00 0 0 0 00 0 0 000 00 0";
 
         [TestMethod]
         public void TestC()
         {
-            Assert.AreEqual(CSOLUTION, Unary.Solution.SayBlah());
+            Assert.AreEqual(CSOLUTION, Unary.Solution.ConvertString(CINPUT));
         }
 
         [TestMethod]
         public void TestCC()
         {
-            Assert.AreEqual(CCSOLUTION, Unary.Solution.SayBlah());
+            Assert.AreEqual(CCSOLUTION, Unary.Solution.ConvertString(CCINPUT));
         }
 
         [TestMethod]
         public void TestPercent()
         {
-            Assert.AreEqual(PERCENTSOLUTION, Unary.Solution.SayBlah());
+            Assert.AreEqual(PERCENTSOLUTION, Unary.Solution.ConvertString(PERCENTINPUT));
         }
 
         [TestMethod]
         public void TestMessage()
         {
-            Assert.AreEqual(MESSAGESOLUTION, Unary.Solution.SayBlah());
+            Assert.AreEqual(MESSAGESOLUTION, Unary.Solution.ConvertString(MESSAGEINPUT));
         }
     }
 }
