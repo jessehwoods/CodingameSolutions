821d191818130502cb675fa7153815ff851e3ffb
Jesse Woods <jessehwoods@gmail.com>
2022-05-17 14:12:22 -0400

    Started another puzzle.

---
+++ ThereIsNoSpoonEpisode1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ThereIsNoSpoonEpisode1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThereIsNoSpoonEpisode1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63990e1b-c0c1-41cb-ba0d-9ad7589fc068")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThereIsNoSpoonEpisode1Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ThereIsNoSpoonEpisode1Tests
+{
+
+    /**
+     * These are tests for a solution to the puzzle at https://www.codingame.com/training/medium/there-is-no-spoon-episode-1
+     */
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
