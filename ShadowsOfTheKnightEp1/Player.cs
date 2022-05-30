bd1bec088e30ba8900046824e05bc70bced65bb0
Jesse Woods <jessehwoods@gmail.com>
2022-05-30 13:41:07 -0400

    Added a link to the problem in new code.

--- ShadowsOfTheKnightEp1/Player.cs
+++ ShadowsOfTheKnightEp1/Player.cs
@@ -5,34 +5,36 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-class Player
+namespace ShadowOfTheKnightEp1
 {
-    static void Main(string[] args)
+    /**
+     * This is a solution to the puzzle at https://www.codingame.com/ide/puzzle/shadows-of-the-knight-episode-1
+     **/
+    class Player
     {
-        string[] inputs;
-        inputs = Console.ReadLine().Split(' ');
-        int W = int.Parse(inputs[0]); // width of the building.
-        int H = int.Parse(inputs[1]); // height of the building.
-        int N = int.Parse(Console.ReadLine()); // maximum number of turns before game over.
-        inputs = Console.ReadLine().Split(' ');
-        int X0 = int.Parse(inputs[0]);
-        int Y0 = int.Parse(inputs[1]);
-
-        // game loop
-        while (true)
+        static void Main(string[] args)
         {
-            string bombDir = Console.ReadLine(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+            int W = int.Parse(inputs[0]); // width of the building.
+            int H = int.Parse(inputs[1]); // height of the building.
+            int N = int.Parse(Console.ReadLine()); // maximum number of turns before game over.
+            inputs = Console.ReadLine().Split(' ');
+            int X0 = int.Parse(inputs[0]);
+            int Y0 = int.Parse(inputs[1]);
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
+            // game loop
+            while (true)
+            {
+                string bombDir = Console.ReadLine(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
 
 
-            // the location of the next window Batman should jump to.
-            Console.WriteLine("0 0");
+                // the location of the next window Batman should jump to.
+                Console.WriteLine("0 0");
+            }
         }
     }
 }
