89eb73a68bc4c33afbc94dd9d99a689a0bce68e0
Jesse Woods <jessehwoods@gmail.com>
2022-05-30 13:46:03 -0400

    Created unit test class for ShadowOfTheKnightEp1

---
+++ ClassLibrary/ShadowOfTheKnightEp1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShadowOfTheKnightEp1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShadowOfTheKnightEp1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ecfb793c-a15d-4633-a395-e394388630b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ClassLibrary/ShadowOfTheKnightEp1Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace ShadowOfTheKnightEp1Tests
+{
+    [TestClass]
+    public class ShadowOfTheKnightEp1Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
